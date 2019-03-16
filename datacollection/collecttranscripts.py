dfaa93651a2b466c5ea6f2283c0fe5a9c45793ca
Carl Cortright <carl@carlcortright.com>
2019-03-16 14:02:02 -0600

    data collection working

--- datacollection/collecttranscripts.py
+++ datacollection/collecttranscripts.py
@@ -1,13 +1,26 @@
-from yaml import load
+import json
+from yaml import load, FullLoader
 from youtube import API
 from youtube_transcript_api import YouTubeTranscriptApi
 
 with open("keys.yaml", "r") as keys:
-    config = load(keys)
+    config = load(keys, Loader=FullLoader)
 api = API(client_id=config['client_id'], client_secret=config["client_secret"], api_key=config["api_key"])
+
+terms = [
+    "linear algebra",
+    "calculus", 
+    "derivative", 
+    "shakespear",
+    "python coding tutorial"
+]
 
 def get_transcript(video_id):
     return YouTubeTranscriptApi.get_transcript(video_id, languages=["en"])
+
+def write_transcript_to_file(name, transcript):
+    with open("transcripts/" + name + '.json', 'w') as writefile:  
+        json.dump(transcript, writefile)
 
 def get_search_ids(term, max_ids=10000):
     ids = list()
@@ -22,11 +35,29 @@ def get_search_ids(term, max_ids=10000):
             maxResults=50,
             pageToken=token
         )
-        token = response["nextPageToken"]
+        try:
+            token = response["nextPageToken"]
+        except KeyError:
+            return set(ids)
         for item in response["items"]:
-            if item["id"]["videoId"] not in ids:
-                ids.append(item["id"]["videoId"])
-    return ids
+            ids.append(item["id"]["videoId"])
+    return set(ids)
+
+def download_transcripts():
+    searched = list()
+    for term in terms:
+        print(f"Exploring term: {term}")
+        ids = get_search_ids(term)
+        for i in ids:
+            if i not in searched:
+                searched.append(i)
+                try:
+                    transcript = get_transcript(i)
+                except YouTubeTranscriptApi.CouldNotRetrieveTranscript: 
+                    print(f"couldn't retrieve transcript for {i}")
+                    continue
+                write_transcript_to_file(i, transcript)
+
 
 if __name__ == "__main__":
-    print(get_search_ids("hello", max_ids=400))
+    download_transcripts()
